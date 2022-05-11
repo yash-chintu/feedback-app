a591b1a36253e0893949e294fa2311009978c4d5
unknown <krishnayaswanth4@gmail.com>
2022-05-11 11:49:32 +0530

    First Commit

---
+++ src/components/Feedbackitem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Feedbackitem() {
+
+    const [rating, setRating] = useState(7);
+    const [text, setText] = useState("LOLOLOL")
+
+
+    return (
+        <div className="card">
+            <div className="num-display">{rating}</div>
+            <div className="text-display">{text}</div>
+        </div>
+    )
+}
+
+export default Feedbackitem
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+function Header({bgColor,textColor,text}) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+
+
+Header.defaultProps = {
+    text : 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
+}
+
+
+
+export default Header
